c2089518dca894f32b775f4e3bb92c621f2e859e
Tadeh <tadeh19@gmail.com>
2021-11-09 17:49:10 +0330

    update app.py file

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-# import pandas as pd
+import pandas as pd
 from flask import Flask, jsonify, request
 import pickle
 
-# model = pickle.load(open('model.pkl','rb'))
-# df_filtered = pd.read_csv('series_data.csv')
-# names_df = pd.read_csv('names.csv')
+model = pickle.load(open('model.pkl','rb'))
+df_filtered = pd.read_csv('series_data.csv')
+names_df = pd.read_csv('names.csv')
 
 # app
 app = Flask(__name__)
@@ -21,24 +21,24 @@ def predict():
             "num_of_recs": 10
         }
     '''
-    # data = request.get_json(force=True)
-    # predict_idx = 512
-    # n_neighbors = 10
+    data = request.get_json(force=True)
+    predict_idx = data['show_id']
+    n_neighbors = data['num_of_recs']
 
-    # similar_shows = model.kneighbors(X=df_filtered.iloc[predict_idx].to_numpy().reshape(1, -1),
-    #                                     n_neighbors=n_neighbors+1, 
-    #                                     return_distance=False
-    #                                 )
+    similar_shows = model.kneighbors(X=df_filtered.iloc[predict_idx].to_numpy().reshape(1, -1),
+                                        n_neighbors=n_neighbors+1, 
+                                        return_distance=False
+                                    )
 
-    # results = []
+    results = []
 
-    # for show_idx in similar_shows[0]:
-    #     if predict_idx != show_idx:
-    #         results.append(names_df.at[show_idx,'id'])
+    for show_idx in similar_shows[0]:
+        if predict_idx != show_idx:
+            results.append(names_df.at[show_idx,'id'])
 
-    # output = {'similar_shows': 'test'}
+    output = {'similar_shows': results}
 
-    return jsonify(results=0)
+    return jsonify(results=output)
 
 if __name__ == '__main__':
     app.run(port = 5000)
