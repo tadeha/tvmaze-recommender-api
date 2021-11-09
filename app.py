fe08e3ba810fd2a741896cb0620ac80c2b970386
Tadeh <tadeh19@gmail.com>
2021-11-09 15:35:15 +0330

    add flask files

---
+++ app.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from flask import Flask, jsonify, request
+import pickle
+
+model = pickle.load(open('model.pkl','rb'))
+df_filtered = pd.read_csv('series_data.csv')
+names_df = pd.read_csv('names.csv')
+
+# app
+app = Flask(__name__)
+
+# routes
+@app.route('/', methods=['POST'])
+
+def predict():
+
+    '''
+        Input Data:
+        {
+            'show_id': 1,
+            'num_of_recs': 5,
+        }
+    '''
+    data = request.get_json(force=True)
+    predict_idx = data['show_id']
+    n_neighbors = data['num_of_recs']
+
+    similar_shows = model.kneighbors(X=df_filtered.iloc[predict_idx].to_numpy().reshape(1, -1), n_neighbors=n_neighbors+1, return_distance=False)
+
+    similar_shows = []
+
+    for show_idx in similar_shows[0]:
+        if predict_idx != show_idx:
+            similar_shows.append(names_df.at[show_idx,'id'])
+
+    output = {'similar_shows': similar_shows}
+
+    return jsonify(results=output)
+
+if __name__ == '__main__':
+    app.run(port = 5000, debug = True)
