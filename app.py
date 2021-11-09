a515af6782638d21823d77a23b2936c277095529
Tadeh <tadeh19@gmail.com>
2021-11-09 18:33:05 +0330

    update app.py file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from flask import Flask, jsonify, request, Response
+from flask import Flask, jsonify, request
 import pickle
 import json
 
@@ -30,7 +30,7 @@ def predict():
         if predict_idx != show_idx:
             results.append(names_df.at[show_idx,'id'])
 
-    return Response(json.dumps(results),  mimetype='application/json')
+    return jsonify(similar_shows=' ,'.join(str(e) for e in results))
 
 if __name__ == '__main__':
     app.run(port = 5000)
