cd73d26edcd4dfdbdd0af32d5619f0cab3825d00
Tadeh <tadeh19@gmail.com>
2021-11-10 14:50:53 +0330

    add weighted files

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ import pickle
 model = pickle.load(open('model.pkl','rb'))
 df_filtered = pd.read_csv('series_data.csv')
 df_filtered = df_filtered.set_index('id')
+
+model_weighted = pickle.load(open('model_weighted.pkl','rb'))
+df_filtered_weighted = pd.read_csv('series_data_weighted.csv')
+df_filtered_weighted = df_filtered_weighted.set_index('id')
+
+
 names_df = pd.read_csv('names.csv')
 
 # app
@@ -18,14 +24,21 @@ def recommend():
     data = request.get_json(force=True)
     predict_idx = data['show_id']
     n_neighbors = data['num_of_recs']
+    weighted = data['weighted_model']
 
     try:
-        results = {}
-        similar_shows = model.kneighbors(X=df_filtered.loc[[predict_idx]].to_numpy().reshape(1, -1),
+        if weighted:
+            similar_shows = model_weighted.kneighbors(X=df_filtered_weighted.loc[[predict_idx]].to_numpy().reshape(1, -1),
                                             n_neighbors=n_neighbors+1,
                                             return_distance=False
                                         )
-
+        else:
+            similar_shows = model.kneighbors(X=df_filtered.loc[[predict_idx]].to_numpy().reshape(1, -1),
+                                            n_neighbors=n_neighbors+1,
+                                            return_distance=False
+                                        )
+        results = {}
+        
         for show_idx in similar_shows[0]:
             if predict_idx != show_idx:
                 results[str(names_df.iloc[[show_idx]].index.tolist()[0])] = names_df.iloc[[show_idx]].values[0][1]
