039fe24a2ab1b3d67c518a73bbdc99af6a02f3c2
Tadeh <tadeh19@gmail.com>
2022-03-02 14:47:04 +0330

    Update app file

--- app.py
+++ app.py
@@ -52,9 +52,9 @@ def recommend():
         """
         results = {}
         df_train = input_df.copy()
-        show = df_train[df_train['id'] == predict_idx].drop(['name', 'weight'], axis=1).set_index('id')
+        show = df_train[df_train['id'] == predict_idx].drop(['name', 'show_rating', 'weight'], axis=1).set_index('id')
         df_train = df_train.set_index('id')
-        df_train_without_metadata = df_train.drop(['name', 'weight'], axis=1)
+        df_train_without_metadata = df_train.drop(['name', 'show_rating', 'weight'], axis=1)
         ary = scipy.spatial.distance.cdist(show, df_train_without_metadata, metric='euclidean')
         similars = df_train[ary[0]==ary[0].min()]
         similars = similars[~similars.index.isin([predict_idx])]
