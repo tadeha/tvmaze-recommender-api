664002e0f4aaa4b067d46b96c518e177d391149c
Tadeh <tadeh19@gmail.com>
2022-03-02 14:39:19 +0330

    add the naive method

--- app.py
+++ app.py
@@ -7,10 +7,13 @@
 import pandas as pd
 from flask import Flask, jsonify, request
 import pickle
+import scipy
+
 
 # Constants
 MIN_YEAR = 2020
-MIN_RATING = 8.5
+MIN_WEIGHT = 70
+RANDOM_STATE = 42
 COLUMNS_TO_REMOVE = ['id', 'name', 'show_rating']
 
 # Variables
@@ -31,6 +34,7 @@ def recommend():
     weighted = data['weighted_model']
 
     try:
+        """
         show = input_df[input_df['id'] == predict_idx].drop(COLUMNS_TO_REMOVE,axis=1).to_numpy().reshape(1, -1)
 
         similar_shows = model.kneighbors(
@@ -45,10 +49,26 @@ def recommend():
             show_id = input_df.iloc[[show_idx]]['id'].values[0]
             if predict_idx != show_id:
                 results[str(show_id)] = input_df.iloc[[show_idx]]['name'].values[0]
-
-    except ValueError:        
+        """
         results = {}
-        df_trend = input_df[input_df['show_rating']>=MIN_RATING].sample(n=n_neighbors)
+        df_train = input_df.copy()
+        show = df_train[df_train['id'] == predict_idx].drop(['name', 'weight'], axis=1).set_index('id')
+        df_train = df_train.set_index('id')
+        df_train_without_metadata = df_train.drop(['name', 'weight'], axis=1)
+        ary = scipy.spatial.distance.cdist(show, df_train_without_metadata, metric='euclidean')
+        similars = df_train[ary[0]==ary[0].min()]
+        similars = similars[~similars.index.isin([predict_idx])]
+        similars = similars.sample(n=n_neighbors, weights=similars.weight, random_state=RANDOM_STATE).reset_index().sort_values('weight', ascending=False)
+
+        for row in similars.iterrows():
+            show_id = row[1]['id']
+            show_name = row[1]['name']
+            results[str(show_id)] = show_name
+
+    # except ValueError:
+    except IndexError:        
+        results = {}
+        df_trend = input_df[input_df['weight']>=MIN_WEIGHT].sample(n=n_neighbors)
 
         for trend in df_trend.iterrows():
             results[str(trend[1]['id'])] = trend[1]['name']
